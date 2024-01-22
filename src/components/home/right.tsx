4896dd29bb354779b93329f90b948354a9b81805
taewoo <rlaxodn322@gmail.com>
2024-01-22 17:19:17 +0900

    home layout 구상

---
+++ src/components/home/right.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Right = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%',
+          height: '100px',
+        }}
+      >
+        <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
+        <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
+        <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
+        <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
+      </div>
+    </>
+  );
+};
+
+export default Right;
--- src/pages/auth/home/index.tsx
+++ src/pages/auth/home/index.tsx
@@ -1,5 +1,5 @@
 import MainLayout from '../../../layouts/index';
-
+import Right from '../../../components/home/right';
 import Map from '../../../components/apis/kakao/map';
 import { Page } from './style';
 const MyPage = () => {
@@ -7,21 +7,14 @@ const MyPage = () => {
     <>
       <Page>
         <div style={{ display: 'flex', height: '750px' }}>
-          <div style={{ width: '50%', height: '100%', marginRight: '10px' }}>
+          <div style={{ width: '100%', height: '100%', marginRight: '10px' }}>
             <Map />
           </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              width: '50%',
-              height: '100px',
-            }}
-          >
-            <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
-            <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
-            <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
-            <div style={{ width: '24%', height: '100px', boxShadow: '2px 2px 2px 2px lightgray' }}></div>
+          <div style={{ width: '100%', height: '100%', marginRight: '10px' }}>
+            <Right />
+            <div
+              style={{ marginTop: '10px', width: '100%', height: '560px', boxShadow: '2px 2px 2px 2px lightgray' }}
+            ></div>
           </div>
         </div>
       </Page>
