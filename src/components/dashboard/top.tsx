0fb54c04ef0d21f70b42bdebaf3da11e4fb0a856
taewoo <rlaxodn322@gmail.com>
2024-01-23 12:00:55 +0900

    배경 색 변경

--- src/components/dashboard/top.tsx
+++ src/components/dashboard/top.tsx
@@ -17,20 +17,53 @@ const Top = () => {
             height: '100%',
             borderRadius: '10px',
             boxShadow: '2px 2px 2px 2px lightgray',
+            background: 'white',
           }}
         >
           <h1 style={{ margin: '20px' }}>등록버스</h1>
         </div>
-        <div style={{ borderRadius: '10px', boxShadow: '2px 2px 2px 2px lightgray', width: '19%', height: '100%' }}>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '2px 2px 2px 2px lightgray',
+            width: '19%',
+            height: '100%',
+            background: 'white',
+          }}
+        >
           <h1 style={{ margin: '20px' }}>운행중</h1>
         </div>
-        <div style={{ borderRadius: '10px', boxShadow: '2px 2px 2px 2px lightgray', width: '19%', height: '100%' }}>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '2px 2px 2px 2px lightgray',
+            width: '19%',
+            height: '100%',
+            background: 'white',
+          }}
+        >
           <h1 style={{ margin: '20px' }}>대기/충전중</h1>
         </div>
-        <div style={{ borderRadius: '10px', boxShadow: '2px 2px 2px 2px lightgray', width: '19%', height: '100%' }}>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '2px 2px 2px 2px lightgray',
+            width: '19%',
+            height: '100%',
+            background: 'white',
+          }}
+        >
           <h1 style={{ margin: '20px' }}>점검/수리중</h1>
         </div>
-        <div style={{ borderRadius: '10px', boxShadow: '2px 2px 2px 2px lightgray', width: '19%', height: '100%' }}>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '2px 2px 2px 2px lightgray',
+            width: '19%',
+            height: '100%',
+            background: 'white',
+          }}
+        >
           <h1 style={{ margin: '20px' }}>배터리정보</h1>
           <div>
             <Mqtt />
