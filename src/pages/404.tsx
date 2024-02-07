1a11869d8c4cba008896cf087b66c2c1f6f1ed8b
taewoo <rlaxodn322@gmail.com>
2024-02-07 14:27:57 +0900

    404페이지

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,4 @@
 import { Icon } from 'semantic-ui-react';
-
 export default function Error404() {
   return (
     <div style={{ padding: '200px 0', textAlign: 'center', fontSize: 30 }}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
 import PageWithLayoutType from '../types/pageWithLayout';
-import { ReactElement, ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { RecoilRoot } from 'recoil';
 import { NextPage } from 'next/types';
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -13,12 +13,9 @@ type AppLayoutProps = AppProps & {
   Component: PageWithLayoutType;
   pageProps: any;
 };
-
 const MyApp = ({ Component, pageProps }: AppLayoutProps) => {
-  const Layout = Component.layout || ((children: ReactElement) => <>{children}</>);
-
-  const queryClient = new QueryClient();
-
+  const Layout = Component.layout || (({ children }: { children: ReactNode }) => <>{children}</>);
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
@@ -32,5 +29,41 @@ const MyApp = ({ Component, pageProps }: AppLayoutProps) => {
     </RecoilRoot>
   );
 };
-
 export default MyApp;
+// import '../styles/globals.css';
+// import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { AppProps } from 'next/app';
+// import PageWithLayoutType from '../types/pageWithLayout';
+// import { ReactElement, ReactNode } from 'react';
+// import { RecoilRoot } from 'recoil';
+// import '../config/recoil';
+// import { NextPage } from 'next/types';
+// export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+//   getLayout?: () => ReactNode;
+// };
+// type AppLayoutProps = AppProps & {
+//   Component: PageWithLayoutType;
+//   pageProps: any;
+// };
+
+// const MyApp = ({ Component, pageProps }: AppLayoutProps) => {
+//   const Layout = Component.layout || ((children: ReactElement) => <>{children}</>);
+
+//   const queryClient = new QueryClient();
+
+//   return (
+//     <RecoilRoot>
+//       <QueryClientProvider client={queryClient}>
+//         <Hydrate state={pageProps.dehydratedState}>
+//           <Layout>
+//             <Component {...pageProps} />
+//           </Layout>
+//         </Hydrate>
+//         <ReactQueryDevtools initialIsOpen={false} />
+//       </QueryClientProvider>
+//     </RecoilRoot>
+//   );
+// };
+
+// export default MyApp;
