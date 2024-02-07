65d830f6bda5e32f0b36614b109d1ff8587178f6
taewoo <rlaxodn322@gmail.com>
2024-02-07 11:43:39 +0900

    CustomIcon

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,9 @@
-import { Icon } from 'semantic-ui-react';
+import { CustomIcon } from '../CustomIcon';
 
 export default function Error404() {
   return (
     <div style={{ padding: '200px 0', textAlign: 'center', fontSize: 30 }}>
-      <Icon name="warning circle" color="red" />
+      <CustomIcon name="warning circle" color="red" />
       404 : 페이지를 찾을 수 없습니다.
     </div>
   );
