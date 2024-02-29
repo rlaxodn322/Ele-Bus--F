72e02af873f2f3eb5e5089f9490d8f324d73bf03
taewoo <rlaxodn322@gmail.com>
2024-02-29 10:18:13 +0900

    bus 추가 빌드중

--- src/components/bus/top.tsx
+++ src/components/bus/top.tsx
@@ -1,12 +1,12 @@
-// import React from 'react';
-// // import BusCard from './buscard';
-// const Top = () => {
-//   return (
-//     <>
-//       <h1>버스 조회/관리</h1>
-//       {/* <BusCard /> */}
-//     </>
-//   );
-// };
+import React from 'react';
+import BusCard from './buscard';
+const Top = () => {
+  return (
+    <>
+      <h1>버스 조회/관리</h1>
+      <BusCard busCount={0} />
+    </>
+  );
+};
 
-// export default Top;
+export default Top;
