8b6e27d06d087c31acebcbb52ca726b053abf599
taewoo <rlaxodn322@gmail.com>
2024-03-14 10:01:10 +0900

    빌드 배포 전 백엔드 연결 작업

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://localhost:4000' : 'http://localhost:3000';
-// export const backUrl =
-//   process.env.NODE_ENV === 'production' ? 'http://ele.firstcorea.com:3000' : 'http://39.135.179:3000';
+// export const backUrl = process.env.NODE_ENV === 'production' ? 'http://localhost:4000' : 'http://localhost:3000';
+export const backUrl =
+  process.env.NODE_ENV === 'production' ? 'http://ele.firstcorea.com:3000' : 'http://39.135.179:3000';
