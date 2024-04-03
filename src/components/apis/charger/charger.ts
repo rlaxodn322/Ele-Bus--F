2e1d78055e4e6da037fde39a7dcb5e31145d15b7
taewoo <rlaxodn322@gmail.com>
2024-04-03 09:20:29 +0900

    charger api

--- src/components/apis/charger/charger.ts
+++ src/components/apis/charger/charger.ts
@@ -6,6 +6,6 @@ axios.defaults.withCredentials = true;
 
 // 내 charger 불러오기 API
 export async function loadChargerAPI() {
-  // console.log(loadChargerAPI);
-  return await axios.get('/api/charger').then((response) => response.data);
+  //console.log(loadChargerAPI);
+  return await axios.get('api/charger').then((response) => response.data);
 }
