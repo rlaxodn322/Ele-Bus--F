3a4871a8e8ad29f840a13c48472d73a8537da573
taewoo <rlaxodn322@gmail.com>
2024-04-12 16:52:52 +0900

    xss 취약점 보완

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
-// const withAntdLess = require('next-plugin-antd-less');
-
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-// module.exports = withAntdLess({
-//   lessVarsFilePath: './src/styles/variables.less',
-//   ...nextConfig,
-//   webpack(config) {
-//     return config;
-//   },
-// });
-
 module.exports = nextConfig;
+// // const withAntdLess = require('next-plugin-antd-less');
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// // module.exports = withAntdLess({
+// //   lessVarsFilePath: './src/styles/variables.less',
+// //   ...nextConfig,
+// //   webpack(config) {
+// //     return config;
+// //   },
+// // });
+
+// module.exports = nextConfig;
